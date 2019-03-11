b8de7d0341967010382eec11b336de8b881d2ad3
Peleus Uhley <puhley@adobe.com>
2019-03-11 10:39:50 -0700

    Bug fix: Correct count returns

--- web_server/routes/tracked_scans.js
+++ web_server/routes/tracked_scans.js
@@ -353,7 +353,7 @@ module.exports = function(envConfig) {
             promise.then(function(data) {
                 if (count && (port === "22" || port === "25" || port === "465")) {
                     res.status(200).json({'count': data.length});
-                } else if (count && port === "443" && req.query.use_port_data === "1") {
+                } else if (count && port === "443" && req.query.use_port_data !== "1") {
                     res.status(200).json({'count': data.length});
                 } else if (count) {
                     res.status(200).json({'count': data});
@@ -516,7 +516,7 @@ module.exports = function(envConfig) {
             }
 
             promise.then(function(data) {
-                if (count && port === "443" && req.query.use_port_data === "1") {
+                if (count && port === "443" && req.query.use_port_data !== "1") {
                     res.status(200).json({'count': data.length});
                 } else if (count) {
                     res.status(200).json({'count': data});
@@ -675,13 +675,13 @@ module.exports = function(envConfig) {
             } else if (port === "80") {
                 promise = zgrab80.getRecordsByZonePromise(req.query.zone, count);
             } else {
-                promise = zgrabPort.getRecordByDomainPromise(req.query.zone, null, count);
+                promise = zgrabPort.getRecordByZonePromise(req.query.zone, null, count);
             }
 
             promise.then(function(data) {
                 if (count && (port === "22" || port === "25" || port === "465")) {
                     res.status(200).json({'count': data.length});
-                } else if (port === "443" && count && req.query.use_port_data === "1") {
+                } else if (port === "443" && count && req.query.use_port_data !== "1") {
                     res.status(200).json({'count': data.length});
                 } else if (count) {
                     res.status(200).json({'count': data});
