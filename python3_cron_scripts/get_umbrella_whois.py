eceb36de1a278855d61d825d8b75409b5061cd8b
Peleus Uhley <puhley@adobe.com>
2020-12-22 18:16:59 -0800

    Add Cisco Umbrella support

---
+++ python3_cron_scripts/get_umbrella_whois.py
@@ -0,0 +1,155 @@
+#!/usr/bin/python3
+
+# Copyright 2020 Adobe. All rights reserved.
+# This file is licensed to you under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License. You may obtain a copy
+# of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+# OF ANY KIND, either express or implied. See the License for the specific language
+# governing permissions and limitations under the License.
+
+"""
+This script will do searches against the Cisco Umbrella whois database
+based on known email addresses. It is used to identify new root domains.
+
+This script requires a Cisco Umbrella subscription.
+"""
+
+import json
+import logging
+import re
+
+from datetime import datetime
+
+from libs3 import MongoConnector, Umbrella, ZoneIngestor, JobsManager
+from libs3.LoggingUtil import LoggingUtil
+
+
+def search_umbrella_by_nameserver(logger, name_server, orgs, umbrella, zi, jobs_manager):
+    """
+    Search Umbrella based on the name server.
+    Double check the response with org and/or email since the zone may be owned by someone else.
+    """
+    logger.info("Searching: " + name_server)
+    results = umbrella.search_by_name_server(name_server)
+
+    if results is None:
+        logger.error("Error querying nameserver: " + name_server)
+        jobs_manager.record_job_error()
+        exit(1)
+
+    logger.info("Results for " + name_server + ": " + str(results[name_server]['totalResults']))
+
+    for entry in results[name_server]['domains']:
+        domain = entry['domain']
+        logger.debug("Checking domain: " + domain)
+
+        if re.match(r"^([0-9]{1,3}\.){3}[0-9]{1,3}\/\d\d$", domain):
+            logger.debug("Matched IP address. Skipping...")
+            continue
+
+        if entry['current'] is False:
+            continue
+
+        whois_result = umbrella.search_by_domain(domain)
+
+        if 'errorMessage' in whois_result:
+            logger.error("Umbrella error message received when searching for domain: " + domain)
+            continue
+
+        if whois_result['registrantOrganization'] not in orgs:
+            logger.warning(domain + " not registered by known org: " + str(whois_result['registrantOrganization']))
+            continue
+
+        zi.add_zone(domain, 'Umbrella')
+
+
+def add_email_domains(logger, results, email, zi, jobs_manager):
+    """
+    Add the root domains identified by an email search.
+    """
+
+    for entry in results[email]['domains']:
+        domain = entry['domain']
+        logger.debug("Checking domain: " + domain)
+
+        if entry['current'] == False:
+            continue
+
+        if re.match(r"^([0-9]{1,3}\.){3}[0-9]{1,3}\/\d\d$", domain):
+            logger.debug("Matched IP address. Skipping...")
+            continue
+
+        zi.add_zone(domain, 'Umbrella')
+
+
+def search_umbrella_by_email(logger, email, umbrella, zi, jobs_manager):
+    """
+    Search Umbrella for records associated with the provided email address.
+    """
+    logger.info("Searching: " + email)
+
+    results = umbrella.search_by_email(email)
+
+    if results is None:
+        logger.error("Error querying email: " + email)
+        jobs_manager.record_job_error()
+        exit(0)
+
+    total_results = int(results[email]['totalResults'])
+    logger.info("Results for " + email + ": " + str(total_results))
+
+    limit = int(results[email]['limit'])
+
+    offset = 0
+    while offset < total_results:
+        add_email_domains(logger, results, email, zi, jobs_manager)
+        offset = offset + limit
+
+        if offset < total_results:
+            results = umbrella.search_umbrella_by_email(email, offset)
+
+
+def main():
+    """
+    Begin Main...
+    """
+    logger = LoggingUtil.create_log(__name__)
+
+    now = datetime.now()
+    print("Starting: " + str(now))
+    logger.info("Starting...")
+
+    mongo_connector = MongoConnector.MongoConnector()
+    umbrella = Umbrella.Umbrella()
+    zi = ZoneIngestor.ZoneIngestor()
+
+    # Obtain the list of known email addresses and name servers from the config collection
+    config_collection = mongo_connector.get_config_connection()
+    res = config_collection.find({})
+
+    jobs_manager = JobsManager.JobsManager(mongo_connector, 'get_umbrella_whois')
+    jobs_manager.record_job_start()
+
+    # Perform a search for each email address
+    for i in range(0, len(res[0]['DNS_Admins'])):
+        search_umbrella_by_email(logger, res[0]['DNS_Admins'][i], umbrella, zi, jobs_manager)
+
+    # Perform a search based on each name server
+    for i in range(0, len(res[0]['Whois_Name_Servers'])):
+        search_umbrella_by_nameserver(logger, res[0]['Whois_Name_Servers'][i], res[0]['Whois_Orgs'], umbrella, zi, jobs_manager)
+
+    # Record status
+    jobs_manager.record_job_complete()
+
+    now = datetime.now()
+    print("Complete: " + str(now))
+    logger.info("Complete.")
+
+
+if __name__ == "__main__":
+    main()
+
+exit(0)
---
+++ python3_cron_scripts/libs3/Umbrella.py
@@ -0,0 +1,209 @@
+#!/usr/bin/python3
+
+# Copyright 2020 Adobe. All rights reserved.
+# This file is licensed to you under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License. You may obtain a copy
+# of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+# OF ANY KIND, either express or implied. See the License for the specific language
+# governing permissions and limitations under the License.
+
+"""
+This module manages interactions with the Cisco Umbrella service.
+
+A paid subscription is required.
+"""
+
+import configparser
+import json
+import logging
+import requests
+import time
+
+from libs3.ConnectorUtil import ConnectorUtil
+
+from requests.adapters import HTTPAdapter
+from urllib3.util import Retry
+
+
+class Umbrella(object):
+    """
+    This class is designed for interacting with Cisco Umbrella.
+    """
+
+    umbrella_config_file = 'connector.config'
+    TOKEN = None
+    URL = "https://investigate.api.umbrella.com/"
+    _logger = None
+
+
+    def _log(self):
+        """
+        Get the log
+        """
+        return logging.getLogger(__name__)
+
+
+    def _init_umbrella(self, config):
+        """
+        Obtain the configuration data
+        """
+        self.URL = ConnectorUtil.get_config_setting(self._logger, config, "Cisco", "umbrella.url", 'str', self.URL)
+        self.TOKEN = ConnectorUtil.get_config_setting(self._logger, config, "Cisco", "umbrella.key")
+
+
+    def __init__(self, config_file="", log_level=None):
+        """
+        Initialize the class
+        """
+        if config_file != "":
+            self.umbrella_config_file = config_file
+
+        self._logger = self._log()
+
+        if log_level is not None:
+            self._logger.setLevel(log_level)
+
+        config = configparser.ConfigParser()
+        list = config.read(self.umbrella_config_file)
+        if len(list) == 0:
+            self._logger.error('Error: Could not find the config file')
+            exit(1)
+
+        self._init_umbrella(config)
+
+
+    def __requests_retry_session(self, retries=5, backoff_factor=7, status_forcelist=[408, 500, 502, 503, 504],session=None,):
+        """
+        A Closure method for this static method.
+        """
+        session = session or requests.Session()
+        retry = Retry(
+            total=retries,
+            read=retries,
+            connect=retries,
+            backoff_factor=backoff_factor,
+            status_forcelist=status_forcelist,
+        )
+        adapter = HTTPAdapter(max_retries=retry)
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
+
+        return session
+
+
+    def search_by_name_server(self, name_server):
+        """
+        Fetches the whois records from Umbrella based on the registered name server.
+        @param name_server The name server to search for in the whois records
+        """
+        path = self.URL + "whois/nameservers/" + name_server
+
+        headers = {'Authorization': 'Bearer ' + self.TOKEN}
+        parameters = {}
+
+        req = self.__requests_retry_session().get(path, params=parameters,
+                           headers = headers, timeout=120)
+
+        try:
+            res = json.loads(req.text)
+        except:
+            return None
+
+        return res
+
+
+    def search_by_name_servers(self, name_server_list):
+        """
+        Fetches the whois records from Umbrella based on the provided registered name server list.
+        @param name_server_list The name servers to search for in the whois records delimited by a comma
+        """
+        path = self.URL + "whois/nameservers"
+
+        headers = {'Authorization': 'Bearer ' + self.TOKEN}
+        parameters = {'nameServerList', name_server_list}
+
+        req = self.__requests_retry_session().get(path, params=parameters,
+                           headers = headers, timeout=120)
+
+        try:
+            res = json.loads(req.text)
+        except:
+            return None
+
+        return res
+
+
+    def search_by_email(self, email, offset=None):
+        """
+        Fetches the whois records from Umbrella based on the registered email.
+        @param email The email to search for in the whois records
+        """
+        path = self.URL + "whois/emails/" + email
+        headers = {'Authorization': 'Bearer ' + self.TOKEN}
+
+        params = {}
+
+        if offset is not None:
+            params['offset'] = offset
+
+        req = self.__requests_retry_session().get(path, params=params,
+                           headers = headers, timeout=120)
+
+        try:
+            res = json.loads(req.text)
+        except:
+            return None
+
+        return res
+
+
+    def search_by_emails(self, email_list, offset=None):
+        """
+        Fetches the whois records from Umbrella based on the registered email list.
+        @param email_list The comma delimited email list to search for in the whois records
+        """
+        path = self.URL + "whois/emails"
+        headers = {'Authorization': 'Bearer ' + self.TOKEN}
+
+        params = {}
+        params['emailList'] = email_list
+
+        if offset is not None:
+            params['offset'] = offset
+
+        req = self.__requests_retry_session().get(path, params=params,
+                           headers = headers, timeout=120)
+
+        try:
+            res = json.loads(req.text)
+        except:
+            return None
+
+        return res
+
+
+    def search_by_domain(self, domain_name, include_history=False, limit=10):
+        """
+        Search Umbrella for a single DNS record
+        """
+        path = self.URL + "/whois/" + domain_name
+        headers = {'Authorization': 'Bearer ' + self.TOKEN}
+
+        params = {}
+
+        if include_history:
+            path = path + "/history"
+            params['limit'] = limit
+
+        req = self.__requests_retry_session().get(path, params=params,
+                           headers = headers, timeout=120)
+
+        try:
+            res = json.loads(req.text)
+        except:
+            return None
+
+        return res
