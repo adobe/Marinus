6e09bd214bb62b7fc3144aa457bcbfe3528fd765
Peleus Uhley <puhley@adobe.com>
2022-03-02 00:45:57 -0800

    Refactoring improvements

--- python3_cron_scripts/libs3/RemoteMongoConnector.py
+++ python3_cron_scripts/libs3/RemoteMongoConnector.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Copyright 2018 Adobe. All rights reserved.
+# Copyright 2022 Adobe. All rights reserved.
 # This file is licensed to you under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License. You may obtain a copy
 # of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -14,18 +14,12 @@
 This module is for handling connections to the remote Mongo server.
 """
 
-
-import configparser
 import logging
-import time
 
-from pymongo import MongoClient
-from pymongo.errors import AutoReconnect
-
-from libs3.ConnectorUtil import ConnectorUtil
+from libs3.MongoConnectorBase import MongoConnectorBase
 
 
-class RemoteMongoConnector(object):
+class RemoteMongoConnector(MongoConnectorBase):
     """
     This class is designed for interacting with the remote MongoDB.
     The remote MongoDB lives in AWS and it is where large jobs are processed.
@@ -42,189 +36,20 @@ class RemoteMongoConnector(object):
         """
         return logging.getLogger(__name__)
 
-    def _init_mongo_connection(self, config):
-        protocol = ConnectorUtil.get_config_setting(
-            self._logger, config, "RemoteMongoDB", "mongo.protocol"
-        )
-        endpoint = ConnectorUtil.get_config_setting(
-            self._logger, config, "RemoteMongoDB", "mongo.host"
-        )
-        path = ConnectorUtil.get_config_setting(
-            self._logger, config, "RemoteMongoDB", "mongo.path"
-        )
-        username = ConnectorUtil.get_config_setting(
-            self._logger, config, "RemoteMongoDB", "mongo.username"
-        )
-        password = ConnectorUtil.get_config_setting(
-            self._logger, config, "RemoteMongoDB", "mongo.password"
-        )
-        cacert = ConnectorUtil.get_config_setting(
-            self._logger, config, "RemoteMongoDB", "mongo.ca_cert"
-        )
-
-        if username != "" and password != "":
-            connection_string = (
-                protocol + username + ":" + password + "@" + endpoint + path
-            )
-        else:
-            connection_string = protocol + endpoint + path
-
-        if cacert != "":
-            client = MongoClient(connection_string, tls=True, tlsCAFile=cacert)
-        else:
-            client = MongoClient(connection_string)
-
-        self.m_connection = client[path[1:]]
-
     def __init__(self, config_file="", log_level=None):
-        if config_file != "":
-            self.mongo_config_file = config_file
-
+        """
+        Initialize the instance
+        """
         self._logger = self._log()
+
         if log_level is not None:
             self._logger.setLevel(log_level)
 
-        config = configparser.ConfigParser()
-        list = config.read(self.mongo_config_file)
-        if len(list) == 0:
-            self._logger.error("Error: Could not find the config file")
-            exit(1)
+        if config_file != "":
+            self.mongo_config_file = config_file
 
-        self._init_mongo_connection(config)
-
-    def perform_find(self, collection, query, filter=None):
-        """
-        This will perform a find with a retry for dropped connections
-        """
-        success = False
-        num_tries = 0
-        while not success:
-            try:
-                if filter is not None:
-                    result = collection.find(query, filter)
-                else:
-                    result = collection.find(query)
-                success = True
-            except AutoReconnect:
-                if num_tries < 5:
-                    self._logger.warning(
-                        "Warning: Failed to connect to the database. Retrying."
-                    )
-                    time.sleep(5)
-                    num_tries = num_tries + 1
-                else:
-                    self._logger.error(
-                        "ERROR: Exceeded the max number of connection attempts to MongoDB!"
-                    )
-                    exit(1)
-
-        return result
-
-    def perform_find_one(self, collection, query, filter=None):
-        """
-        This will perform a find_one with a retry for dropped connections
-        """
-        success = False
-        num_tries = 0
-        while not success:
-            try:
-                if filter is not None:
-                    result = collection.find_one(query, filter)
-                else:
-                    result = collection.find_one(query)
-                success = True
-            except AutoReconnect:
-                if num_tries < 5:
-                    self._logger.warning(
-                        "Warning: Failed to connect to the database. Retrying."
-                    )
-                    time.sleep(5)
-                    num_tries = num_tries + 1
-                else:
-                    self._logger.error(
-                        "ERROR: Exceeded the max number of connection attempts to MongoDB!"
-                    )
-                    exit(1)
-
-        return result
-
-    def perform_count(self, collection, query):
-        """
-        This will perform a find.count() with a retry for dropped connections
-        """
-        success = False
-        num_tries = 0
-        while not success:
-            try:
-                result = collection.count_documents(query)
-                success = True
-            except AutoReconnect:
-                if num_tries < 5:
-                    self._logger.warning(
-                        "Warning: Failed to connect to the database. Retrying."
-                    )
-                    time.sleep(5)
-                    num_tries = num_tries + 1
-                else:
-                    self._logger.error(
-                        "ERROR: Exceeded the max number of connection attempts to MongoDB!"
-                    )
-                    exit(1)
-
-        return result
-
-    def perform_distinct(self, collection, field, query=None):
-        """
-        This will perform a distinct with a retry for dropped connections
-        """
-        success = False
-        num_tries = 0
-        while not success:
-            try:
-                if query is not None:
-                    result = collection.distinct(field, query)
-                else:
-                    result = collection.distinct(field)
-                success = True
-            except AutoReconnect:
-                if num_tries < 5:
-                    self._logger.warning(
-                        "Warning: Failed to connect to the database. Retrying."
-                    )
-                    time.sleep(5)
-                    num_tries = num_tries + 1
-                else:
-                    self._logger.error(
-                        "ERROR: Exceeded the max number of connection attempts to MongoDB!"
-                    )
-                    exit(1)
-
-        return result
-
-    def perform_insert(self, collection, query):
-        """
-        This will perform an insert_one with a retry for dropped connections
-        """
-        success = False
-        num_tries = 0
-        while not success:
-            try:
-                result = collection.insert_one(query)
-                success = True
-            except AutoReconnect:
-                if num_tries < 5:
-                    self._logger.warning(
-                        "Warning: Failed to connect to the database. Retrying."
-                    )
-                    time.sleep(5)
-                    num_tries = num_tries + 1
-                else:
-                    self._logger.error(
-                        "ERROR: Exceeded the max number of connection attempts to MongoDB!"
-                    )
-                    exit(1)
-
-        return result
+        m_base = MongoConnectorBase(config_file, log_level)
+        self.m_connection = m_base.m_connection
 
     def get_results_connection(self):
         """Returns a connection to the results collection in MongoDB"""
