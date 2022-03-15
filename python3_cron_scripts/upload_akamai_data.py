f8215cad7daa2e1b0d0039ccf2229d06d151dadf
Peleus Uhley <puhley@adobe.com>
2022-03-14 18:42:24 -0700

    Cleanup

--- python3_cron_scripts/upload_akamai_data.py
+++ python3_cron_scripts/upload_akamai_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Copyright 2019 Adobe. All rights reserved.
+# Copyright 2022 Adobe. All rights reserved.
 # This file is licensed to you under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License. You may obtain a copy
 # of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -23,77 +23,96 @@ from datetime import datetime
 from libs3 import JobsManager, MongoConnector
 from libs3.LoggingUtil import LoggingUtil
 
-logger = LoggingUtil.create_log(__name__)
 
-mongo_connector = MongoConnector.MongoConnector()
-akamai_collection = mongo_connector.get_akamai_ips_connection()
-jobs_manager = JobsManager.JobsManager(mongo_connector, "upload_akamai_data")
-jobs_manager.record_job_start()
+def main(logger=None):
+    if logger is None:
+        logger = LoggingUtil.create_log(__name__)
 
-# Clear previous data
-akamai_collection.delete_many({})
+    now = datetime.now()
+    print("Starting: " + str(now))
+    logger.info("Starting...")
 
-AKAMAI_DATA = {}
+    mongo_connector = MongoConnector.MongoConnector()
+    akamai_collection = mongo_connector.get_akamai_ips_connection()
+    jobs_manager = JobsManager.JobsManager(mongo_connector, "upload_akamai_data")
+    jobs_manager.record_job_start()
 
-# Record the date that the data was updated.
-AKAMAI_DATA["created"] = datetime.now()
+    # Clear previous data
+    akamai_collection.delete_many({})
 
-# Leave a note for those who query the database directly.
-AKAMAI_DATA["note"] = (
-    "This does not cover all Akamai ranges. "
-    + "It just covers the ones that the tracked organization appears to use as of the created date."
-)
+    AKAMAI_DATA = {}
 
-# Create the list of known ranges.
-AKAMAI_DATA["ranges"] = []
-AKAMAI_DATA["ranges"].append(
-    {"cidr": "2.23.144.0/20", "ip_range": "2.23.144.0 - 2.23.159.255"}
-)
-AKAMAI_DATA["ranges"].append(
-    {"cidr": "104.64.0.0/10", "ip_range": "104.64.0.0 - 104.127.255.255"}
-)
-AKAMAI_DATA["ranges"].append(
-    {"cidr": "172.224.0.0/12", "ip_range": "172.224.0.0 - 172.239.255.255"}
-)
-AKAMAI_DATA["ranges"].append(
-    {"cidr": "173.222.0.0/15", "ip_range": "173.222.0.0 - 173.223.255.255"}
-)
-AKAMAI_DATA["ranges"].append(
-    {"cidr": "184.24.0.0/13", "ip_range": "184.24.0.0 - 184.31.255.255"}
-)
-AKAMAI_DATA["ranges"].append(
-    {"cidr": "184.50.0.0/15", "ip_range": "184.50.0.0 - 184.51.255.255"}
-)
-AKAMAI_DATA["ranges"].append(
-    {"cidr": "184.84.0.0/14", "ip_range": "184.84.0.0 - 184.87.255.255"}
-)
-AKAMAI_DATA["ranges"].append(
-    {"cidr": "23.0.0.0/12", "ip_range": "23.0.0.0 - 23.15.255.255"}
-)
-AKAMAI_DATA["ranges"].append(
-    {"cidr": "23.32.0.0/11", "ip_range": "23.32.0.0 - 23.63.255.255"}
-)
-AKAMAI_DATA["ranges"].append(
-    {"cidr": "23.64.0.0/14", "ip_range": "23.64.0.0 - 23.67.255.255"}
-)
-AKAMAI_DATA["ranges"].append(
-    {"cidr": "23.72.0.0/13", "ip_range": "23.72.0.0 - 23.79.255.255"}
-)
-AKAMAI_DATA["ranges"].append(
-    {"cidr": "23.192.0.0/11", "ip_range": "23.192.0.0 - 23.223.255.255"}
-)
+    # Record the date that the data was updated.
+    AKAMAI_DATA["created"] = datetime.now()
 
-AKAMAI_DATA["ipv6_ranges"] = []
-AKAMAI_DATA["ipv6_ranges"].append(
-    {
-        "cidr": "2600:1400::/24",
-        "ipv6_range": "2600:1400:: - 2600:14FF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF",
-    }
-)
+    # Leave a note for those who query the database directly.
+    AKAMAI_DATA["note"] = (
+        "This does not cover all Akamai ranges. "
+        + "It just covers the ones that the tracked organization appears to use as of the created date."
+    )
 
-# Insert the data
-mongo_connector.perform_insert(akamai_collection, AKAMAI_DATA)
+    # Create the list of known ranges.
+    AKAMAI_DATA["ranges"] = []
+    AKAMAI_DATA["ranges"].append(
+        {"cidr": "2.23.144.0/20", "ip_range": "2.23.144.0 - 2.23.159.255"}
+    )
+    AKAMAI_DATA["ranges"].append(
+        {"cidr": "104.64.0.0/10", "ip_range": "104.64.0.0 - 104.127.255.255"}
+    )
+    AKAMAI_DATA["ranges"].append(
+        {"cidr": "172.224.0.0/12", "ip_range": "172.224.0.0 - 172.239.255.255"}
+    )
+    AKAMAI_DATA["ranges"].append(
+        {"cidr": "173.222.0.0/15", "ip_range": "173.222.0.0 - 173.223.255.255"}
+    )
+    AKAMAI_DATA["ranges"].append(
+        {"cidr": "184.24.0.0/13", "ip_range": "184.24.0.0 - 184.31.255.255"}
+    )
+    AKAMAI_DATA["ranges"].append(
+        {"cidr": "184.50.0.0/15", "ip_range": "184.50.0.0 - 184.51.255.255"}
+    )
+    AKAMAI_DATA["ranges"].append(
+        {"cidr": "184.84.0.0/14", "ip_range": "184.84.0.0 - 184.87.255.255"}
+    )
+    AKAMAI_DATA["ranges"].append(
+        {"cidr": "23.0.0.0/12", "ip_range": "23.0.0.0 - 23.15.255.255"}
+    )
+    AKAMAI_DATA["ranges"].append(
+        {"cidr": "23.32.0.0/11", "ip_range": "23.32.0.0 - 23.63.255.255"}
+    )
+    AKAMAI_DATA["ranges"].append(
+        {"cidr": "23.64.0.0/14", "ip_range": "23.64.0.0 - 23.67.255.255"}
+    )
+    AKAMAI_DATA["ranges"].append(
+        {"cidr": "23.72.0.0/13", "ip_range": "23.72.0.0 - 23.79.255.255"}
+    )
+    AKAMAI_DATA["ranges"].append(
+        {"cidr": "23.192.0.0/11", "ip_range": "23.192.0.0 - 23.223.255.255"}
+    )
 
-jobs_manager.record_job_complete()
+    AKAMAI_DATA["ipv6_ranges"] = []
+    AKAMAI_DATA["ipv6_ranges"].append(
+        {
+            "cidr": "2600:1400::/24",
+            "ipv6_range": "2600:1400:: - 2600:14FF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF",
+        }
+    )
 
-exit(0)
+    # Insert the data
+    mongo_connector.perform_insert(akamai_collection, AKAMAI_DATA)
+
+    jobs_manager.record_job_complete()
+
+    now = datetime.now()
+    print("Complete: " + str(now))
+    logger.info("Complete.")
+
+
+if __name__ == "__main__":
+    logger = LoggingUtil.create_log(__name__)
+
+    try:
+        main(logger)
+    except Exception as e:
+        logger.error("FATAL: " + str(e), exc_info=True)
+        exit(1)
