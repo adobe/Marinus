8864fcd3f6f8dcc9f5dbebf356cbabbe38228516
Peleus Uhley <puhley@adobe.com>
2022-08-15 00:55:01 -0700

    Add new logging format

--- python3_cron_scripts/remove_fixed_dead_dns_records.py
+++ python3_cron_scripts/remove_fixed_dead_dns_records.py
@@ -19,23 +19,21 @@ Periodically, DNS records are cleaned up and the record for foo.example.org is d
 Once foo.example.org is deleted, we can remove it from the Dead DNS tracking list.
 """
 
-import json
 import logging
 import time
 from datetime import datetime
 
-import requests
 from bson.objectid import ObjectId
-
 from libs3 import DNSManager, GoogleDNS, JobsManager, MongoConnector
 from libs3.LoggingUtil import LoggingUtil
 
 
-def main():
+def main(logger=None):
     """
     Begin Main...
     """
-    logger = LoggingUtil.create_log(__name__)
+    if logger is None:
+        logger = LoggingUtil.create_log(__name__)
 
     now = datetime.now()
     print("Starting: " + str(now))
@@ -66,4 +64,10 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    logger = LoggingUtil.create_log(__name__)
+
+    try:
+        main(logger)
+    except Exception as e:
+        logger.error("FATAL: " + str(e), exc_info=True)
+        exit(1)
