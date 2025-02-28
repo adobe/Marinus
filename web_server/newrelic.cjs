eae7b3415a95154037a6e7b6874ebf554b984fcb
Peleus Uhley <1693373+puhley@users.noreply.github.com>
2025-02-28 14:35:05 -0800

    Migrate to standard New Relic ENV

--- web_server/newrelic.cjs
+++ web_server/newrelic.cjs
@@ -26,7 +26,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: process.env.NEW_RELIC_KEY,
+  license_key: process.env.NEW_RELIC_LICENSE_KEY,
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
@@ -35,4 +35,32 @@ exports.config = {
      */
     level: 'info',
   },
+  /**                                                                            
+   * When true, all request headers except for those listed in attributes.exclude
+   * will be captured for all traces, unless otherwise specified in a destination's
+   * attributes include/exclude lists.                                             
+   */
+  allow_all_headers: true,
+  attributes: {
+    /**                                                                            
+     * Prefix of attributes to exclude from all destinations. Allows * as wildcard 
+     * at end.                                                                     
+     *                                                                             
+     * NOTE: If excluding headers, they must be in camelCase form to be filtered.  
+     *                                                                             
+     * @env NEW_RELIC_ATTRIBUTES_EXCLUDE                                           
+     */
+    exclude: [
+      'request.headers.cookie',
+      'request.headers.authorization',
+      'request.headers.proxyAuthorization',
+      'request.headers.setCookie*',
+      'request.headers.x*',
+      'response.headers.cookie',
+      'response.headers.authorization',
+      'response.headers.proxyAuthorization',
+      'response.headers.setCookie*',
+      'response.headers.x*'
+    ]
+  }
 };
